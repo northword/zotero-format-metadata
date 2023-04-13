d8e68ee69cc8bb25ebd513ddd5451203e789fdb8
Northword <northword@outlook.com>
2023-04-13 15:39:41 +0800

    fix(abbr): change order of abbr matching

--- src/data/journal-abbr/jabref-abbr/generate-journal-list-dot.py
+++ src/data/journal-abbr/jabref-abbr/generate-journal-list-dot.py
@@ -17,39 +17,54 @@ Output: writes file 'journal-list-dots.ts'
 """
 
 import json
+from pathlib import Path
 import sys
 
+base_path = Path(__file__).parent
+
 import_order = [
-  'abbrv.jabref.org/journals/journal_abbreviations_acs.csv',
-  'abbrv.jabref.org/journals/journal_abbreviations_ams.csv',
-  'abbrv.jabref.org/journals/journal_abbreviations_general.csv',
-  'abbrv.jabref.org/journals/journal_abbreviations_geology_physics.csv',
-  'abbrv.jabref.org/journals/journal_abbreviations_ieee.csv',
-  'abbrv.jabref.org/journals/journal_abbreviations_lifescience.csv',
-  'abbrv.jabref.org/journals/journal_abbreviations_mathematics.csv',
-  'abbrv.jabref.org/journals/journal_abbreviations_mechanical.csv',
-  'abbrv.jabref.org/journals/journal_abbreviations_meteorology.csv',
-  'abbrv.jabref.org/journals/journal_abbreviations_sociology.csv',
-  'abbrv.jabref.org/journals/journal_abbreviations_webofscience-dots.csv'
+    'abbrv.jabref.org/journals/journal_abbreviations_acs.csv',
+    'abbrv.jabref.org/journals/journal_abbreviations_ams.csv',
+    'abbrv.jabref.org/journals/journal_abbreviations_general.csv',
+    'abbrv.jabref.org/journals/journal_abbreviations_geology_physics.csv',
+    'abbrv.jabref.org/journals/journal_abbreviations_ieee.csv',
+    'abbrv.jabref.org/journals/journal_abbreviations_lifescience.csv',
+    'abbrv.jabref.org/journals/journal_abbreviations_mathematics.csv',
+    'abbrv.jabref.org/journals/journal_abbreviations_mechanical.csv',
+    'abbrv.jabref.org/journals/journal_abbreviations_meteorology.csv',
+    'abbrv.jabref.org/journals/journal_abbreviations_sociology.csv',
+    'abbrv.jabref.org/journals/journal_abbreviations_webofscience-dots.csv'
 ]
 
 if len(sys.argv) == 1:
-    out_file = 'journal-list-dots.ts'
+    out_file = base_path.joinpath('journal-list-dots.ts')
 else:
-   out_file = sys.argv[1]
+    out_file = base_path.joinpath(sys.argv[1])
 print(f"Writing : {out_file}")
 
 journal_dict = {}
 
 for in_file in import_order:
     count = 0
-    f = open(in_file, "r")
+    f = open(base_path.joinpath(in_file), "r")
     for line in f:
+        # 排除不存在缩写的、注释和不以字母开头的行
         if ";" in line and line[0] != "#" and line[0][0].isalpha():
             count += 1
             parts = line.partition(";")
-            journal_dict[parts[0].strip().lower()] = parts[2].partition(";")[0].strip()
-            # print(count, parts, parts[0].strip(), parts[1].strip(), parts[2].strip(), line.strip() )
+            key = parts[0].strip().lower()
+            current_abbr = parts[2].partition(";")[0].strip()
+            # 排除过长的和过短的期刊
+            if (len(key) <= 80 or len(key) >= 3):
+                # 重复期刊检查，优先使用先匹配到的
+                if (key not in journal_dict):
+                    journal_dict[key] = current_abbr
+                else:
+                    pass
+                    # if len(journal_dict[key]) == len(current_abbr):
+                    #     pass
+                    # else:
+                    #     pass
     f.close()
     print(f"{in_file}: {count}")
 
