06d3a49eb1b7850e8ef7fac36e97d28e26927a76
Northword <northword@outlook.com>
2023-04-26 12:35:38 +0800

    fix: use npm package instead of local for deps

--- typing/global.d.ts
+++ typing/global.d.ts
@@ -10,8 +10,8 @@ declare const _globalThis: {
 };
 
 // declare const ztoolkit: import("../src/addon").MyToolkit;
-// declare const ztoolkit: import("zotero-plugin-toolkit").ZoteroToolkit;
-declare const ztoolkit: import("../../zotero-plugin-toolkit/dist/index").ZoteroToolkit;
+declare const ztoolkit: import("zotero-plugin-toolkit").ZoteroToolkit;
+// declare const ztoolkit: import("../../zotero-plugin-toolkit/dist/index").ZoteroToolkit;
 
 declare const rootURI: string;
 
