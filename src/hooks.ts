northword/zotero-format-metadata

commit 4d1424a9379961a2ff6ba6c9db06bfcc70f6e023

fix: mainWindowLoad bug on macOS

src/hooks.ts
@@ -9,28 +9,34 @@ import { registerShortcuts } from "./modules/shortcuts";
 // import { registerPrompt } from "./modules/prompt";
 import { richTextToolBar } from "./modules/views/richTextToolBar";
 import { setLanguageManualDialog } from "./modules/views/setLanguageManualDialog";
+import { createZToolkit } from "./utils/ztoolkit";
 
 async function onStartup() {
     await Promise.all([Zotero.initializationPromise, Zotero.unlockPromise, Zotero.uiReadyPromise]);
     initLocale();
-    ztoolkit.ProgressWindow.setIconURI("default", `chrome://${config.addonRef}/content/icons/favicon.png`);
-
     registerPrefs();
-
     registerNotifier();
+    await onMainWindowLoad(window);
+}
+
+async function onMainWindowLoad(win: Window): Promise<void> {
+    // Create ztoolkit for every window
+    addon.data.ztoolkit = createZToolkit();
     registerMutationObserver();
-
     registerShortcuts();
-
     registerMenu();
     registerTextTransformMenu();
 }
 
-function onShutdown() {
+async function onMainWindowUnload(win: Window): Promise<void> {
     ztoolkit.unregisterAll();
     Object.values(addon.data.dialogs).forEach((dialog) => {
         dialog === undefined ? "" : dialog?.window.close();
     });
+}
+
+function onShutdown() {
+    onMainWindowUnload(window);
     // addon.data.dialogs = {};
     // Remove addon object
     addon.data.alive = false;

addon/bootstrap.js
@@ -5,66 +5,12 @@
  * [2] https://www.zotero.org/support/dev/zotero_7_for_developers
  */
 
-if (typeof Zotero == "undefined") {
-    var Zotero;
-}
-
 var chromeHandle;
-
-// In Zotero 6, bootstrap methods are called before Zotero is initialized, and using include.js
-// to get the Zotero XPCOM service would risk breaking Zotero startup. Instead, wait for the main
-// Zotero window to open and get the Zotero object from there.
-//
-// In Zotero 7, bootstrap methods are not called until Zotero is initialized, and the 'Zotero' is
-// automatically made available.
-async function waitForZotero() {
-    if (typeof Zotero != "undefined") {
-        await Zotero.initializationPromise;
-    }
-
-    var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
-    var windows = Services.wm.getEnumerator("navigator:browser");
-    var found = false;
-    while (windows.hasMoreElements()) {
-        let win = windows.getNext();
-        if (win.Zotero) {
-            Zotero = win.Zotero;
-            found = true;
-            break;
-        }
-    }
-    if (!found) {
-        await new Promise((resolve) => {
-            var listener = {
-                onOpenWindow: function (aWindow) {
-                    // Wait for the window to finish loading
-                    let domWindow = aWindow
-                        .QueryInterface(Ci.nsIInterfaceRequestor)
-                        .getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
-                    domWindow.addEventListener(
-                        "load",
-                        function () {
-                            domWindow.removeEventListener("load", arguments.callee, false);
-                            if (domWindow.Zotero) {
-                                Services.wm.removeListener(listener);
-                                Zotero = domWindow.Zotero;
-                                resolve();
-                            }
-                        },
-                        false,
-                    );
-                },
-            };
-            Services.wm.addListener(listener);
-        });
-    }
-    await Zotero.initializationPromise;
-}
 
 function install(data, reason) {}
 
 async function startup({ id, version, resourceURI, rootURI }, reason) {
-    await waitForZotero();
+    await Zotero.initializationPromise;
 
     // String 'rootURI' introduced in Zotero 7
     if (!rootURI) {
@@ -91,19 +37,25 @@ async function startup({ id, version, resourceURI, rootURI }, reason) {
     Services.scriptloader.loadSubScript(`${rootURI}/chrome/content/scripts/__addonRef__.js`, ctx);
 }
 
+async function onMainWindowLoad({ window }, reason) {
+    Zotero.__addonInstance__?.hooks.onMainWindowLoad(window);
+}
+
+async function onMainWindowUnload({ window }, reason) {
+    Zotero.__addonInstance__?.hooks.onMainWindowUnload(window);
+}
+
 function shutdown({ id, version, resourceURI, rootURI }, reason) {
     if (reason === APP_SHUTDOWN) {
         return;
     }
-    if (reason == ADDON_DISABLE) {
-        Services.obs.notifyObservers(null, "startupcache-invalidate", null);
-    }
+
     if (typeof Zotero === "undefined") {
         Zotero = Components.classes["@zotero.org/Zotero;1"].getService(
             Components.interfaces.nsISupports,
         ).wrappedJSObject;
     }
-    Zotero.__addonInstance__.hooks.onShutdown();
+    Zotero.__addonInstance__?.hooks.onShutdown();
 
     Cc["@mozilla.org/intl/stringbundle;1"].getService(Components.interfaces.nsIStringBundleService).flushBundles();
 
@@ -116,27 +68,3 @@ function shutdown({ id, version, resourceURI, rootURI }, reason) {
 }
 
 function uninstall(data, reason) {}
-
-// Loads default preferences from defaults/preferences/prefs.js in Zotero 6
-function setDefaultPrefs(rootURI) {
-    var branch = Services.prefs.getDefaultBranch("");
-    var obj = {
-        pref(pref, value) {
-            switch (typeof value) {
-                case "boolean":
-                    branch.setBoolPref(pref, value);
-                    break;
-                case "string":
-                    branch.setStringPref(pref, value);
-                    break;
-                case "number":
-                    branch.setIntPref(pref, value);
-                    break;
-                default:
-                    Zotero.logError(`Invalid type '${typeof value}' for pref '${pref}'`);
-            }
-        },
-    };
-    Zotero.getMainWindow().console.log(rootURI + "prefs.js");
-    Services.scriptloader.loadSubScript(rootURI + "prefs.js", obj);
-}

src/addon.ts
@@ -1,8 +1,6 @@
 import { DialogHelper } from "zotero-plugin-toolkit/dist/helpers/dialog";
 import hooks from "./hooks";
-import ZoteroToolkit from "zotero-plugin-toolkit/dist/index";
-// 临时使用本地 toolkit .
-// import ZoteroToolkit from "../../zotero-plugin-toolkit/dist/index";
+import { createZToolkit } from "./utils/ztoolkit";
 
 class Addon {
     public data: {
@@ -10,7 +8,7 @@ class Addon {
         // Env type, see build.js
         env: "development" | "production";
         // ztoolkit: MyToolkit;
-        ztoolkit: ZoteroToolkit;
+        ztoolkit: ZToolkit;
         locale?: {
             current: any;
         };
@@ -32,7 +30,7 @@ class Addon {
             alive: true,
             env: __env__,
             // ztoolkit: new MyToolkit(),
-            ztoolkit: new ZoteroToolkit(),
+            ztoolkit: createZToolkit(),
             dialogs: {},
         };
         this.hooks = hooks;

src/index.ts
@@ -1,5 +1,5 @@
-import Addon from "./addon";
 import { BasicTool } from "zotero-plugin-toolkit/dist/basic";
+import Addon from "./addon";
 import { config } from "../package.json";
 
 const basicTool = new BasicTool();
@@ -7,19 +7,26 @@ const basicTool = new BasicTool();
 if (!basicTool.getGlobal("Zotero")[config.addonInstance]) {
     // Set global variables
     _globalThis.Zotero = basicTool.getGlobal("Zotero");
-    _globalThis.ZoteroPane = basicTool.getGlobal("ZoteroPane");
-    _globalThis.Zotero_Tabs = basicTool.getGlobal("Zotero_Tabs");
-    _globalThis.window = basicTool.getGlobal("window");
+    defineGlobal("window");
+    defineGlobal("document");
     _globalThis.console = basicTool.getGlobal("window").console;
-    _globalThis.document = basicTool.getGlobal("document");
+    defineGlobal("ZoteroPane");
+    defineGlobal("Zotero_Tabs");
     _globalThis.addon = new Addon();
-    _globalThis.ztoolkit = addon.data.ztoolkit;
-    ztoolkit.basicOptions.log.prefix = `[${config.addonName}]`;
-    ztoolkit.basicOptions.log.disableConsole = addon.data.env === "production";
-    ztoolkit.UI.basicOptions.ui.enableElementJSONLog = addon.data.env === "development";
-    ztoolkit.UI.basicOptions.ui.enableElementDOMLog = addon.data.env === "development";
-    ztoolkit.basicOptions.debug.disableDebugBridgePassword = addon.data.env === "development";
+    defineGlobal("ztoolkit", () => {
+        return _globalThis.addon.data.ztoolkit;
+    });
     Zotero[config.addonInstance] = addon;
     // Trigger addon hook for initialization
     addon.hooks.onStartup();
+}
+
+function defineGlobal(name: Parameters<BasicTool["getGlobal"]>[0]): void;
+function defineGlobal(name: string, getter: () => any): void;
+function defineGlobal(name: string, getter?: () => any) {
+    Object.defineProperty(_globalThis, name, {
+        get() {
+            return getter ? getter() : basicTool.getGlobal(name);
+        },
+    });
 }

src/utils/ztoolkit.ts
@@ -1,0 +1,47 @@
+import ZoteroToolkit from "zotero-plugin-toolkit";
+// import ZoteroToolkit from "../../zotero-plugin-toolkit/dist/index";
+
+import { config } from "../../package.json";
+
+export { createZToolkit };
+
+function createZToolkit() {
+    const _ztoolkit = new ZoteroToolkit();
+    /**
+     * Alternatively, import toolkit modules you use to minify the plugin size.
+     * You can add the modules under the `MyToolkit` class below and uncomment the following line.
+     */
+    // const _ztoolkit = new MyToolkit();
+    initZToolkit(_ztoolkit);
+    return _ztoolkit;
+}
+
+function initZToolkit(_ztoolkit: ReturnType<typeof createZToolkit>) {
+    const env = __env__;
+    _ztoolkit.basicOptions.log.prefix = `[${config.addonName}]`;
+    _ztoolkit.basicOptions.log.disableConsole = env === "production";
+    _ztoolkit.UI.basicOptions.ui.enableElementJSONLog = __env__ === "development";
+    _ztoolkit.UI.basicOptions.ui.enableElementDOMLog = __env__ === "development";
+    _ztoolkit.basicOptions.debug.disableDebugBridgePassword = __env__ === "development";
+    _ztoolkit.basicOptions.api.pluginID = config.addonID;
+    _ztoolkit.ProgressWindow.setIconURI("default", `chrome://${config.addonRef}/content/icons/favicon.png`);
+}
+
+import { BasicTool, unregister } from "zotero-plugin-toolkit/dist/basic";
+import { UITool } from "zotero-plugin-toolkit/dist/tools/ui";
+import { PreferencePaneManager } from "zotero-plugin-toolkit/dist/managers/preferencePane";
+
+class MyToolkit extends BasicTool {
+    UI: UITool;
+    PreferencePane: PreferencePaneManager;
+
+    constructor() {
+        super();
+        this.UI = new UITool(this);
+        this.PreferencePane = new PreferencePaneManager(this);
+    }
+
+    unregisterAll() {
+        unregister(this);
+    }
+}

typings/global.d.ts
@@ -1,20 +1,22 @@
 declare const _globalThis: {
-    [key: string]: unknown;
+    [key: string]: any;
     Zotero: _ZoteroTypes.Zotero;
     ZoteroPane: _ZoteroTypes.ZoteroPane;
     Zotero_Tabs: typeof Zotero_Tabs;
     window: Window;
     document: Document;
-    ztoolkit: typeof ztoolkit;
+    ztoolkit: ZToolkit;
     addon: typeof addon;
 };
 
-// declare const ztoolkit: import("../src/addon").MyToolkit;
-declare const ztoolkit: import("zotero-plugin-toolkit").ZoteroToolkit;
-// declare const ztoolkit: import("../../zotero-plugin-toolkit/dist/index").ZoteroToolkit;
+declare type ZToolkit = ReturnType<typeof import("../src/utils/ztoolkit").createZToolkit>;
+
+declare const ztoolkit: ZToolkit;
 
 declare const rootURI: string;
 
 declare const addon: import("../src/addon").default;
 
 declare const __env__: "production" | "development";
+
+declare class Localization {}

commit 1d006ee23e3f25cd972937cf7f9ee68af67d6140

fix: mainWindowLoad bug introduced in 4d1424a9379961a2ff6ba6c9db06bfcc70f6e023

src/hooks.ts
@@ -282,6 +282,8 @@ async function onUpdateInBatch(mode: string, items: Zotero.Item[] | "item" | "co
 
 export default {
     onStartup,
+    onMainWindowLoad,
+    onMainWindowUnload,
     onShutdown,
     onNotify,
     onMutationObserver,