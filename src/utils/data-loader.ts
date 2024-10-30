c89c2d45b3c3c9b2ac0c3002c98ed267e8ef1fcb
Northword <northword@outlook.com>
2024-10-30 11:17:11 +0800

    fix: do not check surffix when load data
    fix: #247

--- src/utils/data-loader.ts
+++ src/utils/data-loader.ts
@@ -7,7 +7,7 @@ export interface Data {
 export async function useData(key: "journalAbbr" | "conferencesAbbr" | "universityPlace" | "iso6393To6391"): Promise<Data>;
 export async function useData(key: "json", path: string): Promise<Data>;
 export async function useData(key: "csv", path: string, loaderOptions: Parameters<typeof csv>[0]): Promise<any[]>;
-export async function useData(key: "txt", path: string): Promise<any>;
+export async function useData(key: "txt", path: string): Promise<string>;
 export async function useData(key: string, path: string): Promise<any>;
 export async function useData(key: string, path?: string, loaderOptions?: any) {
   switch (key) {
@@ -38,19 +38,22 @@ export async function useData(key: string, path?: string, loaderOptions?: any) {
   }
   ztoolkit.log(`${path} data: ${data}`);
 
-  if (key === "csv" && path.endsWith(".csv")) {
-    return await csv({
+  if (key === "csv") {
+    return (await csv({
       delimiter: "auto",
       trim: true,
       noheader: true,
       ...loaderOptions,
-    }).fromString(data);
+    }).fromString(data));
   }
-  else if (key === "json" && path.endsWith(".json")) {
+  else if (key === "json") {
     if (typeof data !== "string" || data === "") {
       throw new SyntaxError("The custom json data file format error.");
     }
     return JSON.parse(data);
+  }
+  else if (key === "txt") {
+    return data;
   }
   else {
     return data;
