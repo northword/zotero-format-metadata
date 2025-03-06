ffed23001b55096933581c977c53b61ae59602ab
Northword <northword@outlook.com>
2025-03-06 21:59:24 +0800

    feat: get pages from pdf to full `pages` field
    resolve: #278

--- src/modules/rules/field-pages.ts
+++ src/modules/rules/field-pages.ts
@@ -8,10 +8,42 @@ export class PagesConnector extends RuleBase<RuleBaseOptions> {
     super(options);
   }
 
-  apply(item: Zotero.Item): Zotero.Item | Promise<Zotero.Item> {
+  async apply(item: Zotero.Item): Promise<Zotero.Item> {
     const pages = item.getField("pages");
-    const newPages = pages.replace(/~/g, "-").replace(/\+/g, ", ");
+    let newPages = this.normizePages(pages);
+
+    if (!this.isPagesRange(pages)) {
+      const numberOfPages = await this.getPDFPages(item);
+      if (numberOfPages) {
+        newPages = `${pages}-${Number(pages) + numberOfPages}`;
+      }
+    }
+
     item.setField("pages", newPages);
     return item;
+  }
+
+  isPagesRange(pages: string): boolean {
+    return pages.includes("-") || pages.includes(",");
+  }
+
+  normizePages(pages: string): string {
+    return pages.replace(/~/g, "-").replace(/\+/g, ", ");
+  }
+
+  async getPDFPages(item: Zotero.Item): Promise<number | void> {
+    const attachment = await item.getBestAttachment();
+    if (!attachment)
+      return;
+
+    if (attachment.attachmentContentType !== "application/pdf")
+      return;
+
+    // @ts-expect-error Fulltext is not in the types
+    const pages = await Zotero.Fulltext.getPages(attachment.id);
+    if (!pages)
+      return;
+
+    return pages.total;
   }
 }
