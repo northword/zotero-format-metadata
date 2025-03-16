c07139d2157c6e903088d9caec2c00f90e78f8e5
Northword <northword@outlook.com>
2025-03-16 15:24:46 +0800

    test: add zotero-plugin test

---
+++ test/abbr.spec.ts
@@ -0,0 +1,9 @@
+import { UpdateAbbr } from "../src/modules/rules/field-abbr";
+
+describe("abbr", () => {
+  it("should have plugin instance defined", async () => {
+    const _item = new Zotero.Item();
+    const item = await new UpdateAbbr({}).apply(_item);
+    console.log(item);
+  });
+});
