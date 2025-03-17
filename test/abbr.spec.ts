6f392f798b2b31060062cc4d9ff6b359b74ce4cb
Northword <northword@outlook.com>
2025-03-17 21:18:24 +0800

    trige test

--- test/abbr.spec.ts
+++ test/abbr.spec.ts
@@ -4,7 +4,6 @@ describe("abbr", () => {
   it("should have plugin instance defined", async () => {
     const _item = new Zotero.Item();
     const item = await new UpdateAbbr({}).apply(_item);
-    console.log(item);
     // expect(1 + 1).to.equal(3);
   });
 });
