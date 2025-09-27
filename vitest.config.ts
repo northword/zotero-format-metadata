bd5421afe9aa4241dc04b538f217000c4a95dcdf
Northword <northword@outlook.com>
2025-09-27 16:29:40 +0800

    test: add coverage script

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,17 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     include: ["**/*.test.ts"],
+    coverage: {
+      provider: "v8",
+      include: [
+        "src/modules/rules/*.ts",
+      ],
+      exclude: [
+        "src/modules/rules/_*.ts",
+        "src/modules/rules/index.ts",
+        "src/modules/rules/rule-base.ts",
+      ],
+      reporter: ["json-summary", "text", "html"],
+    },
   },
 });
