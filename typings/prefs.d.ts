065c4706164e95ebe743471ca015c3196cfb3938
Northword <northword@outlook.com>
2025-10-30 21:51:30 +0800

    chore: update toolkit

--- typings/prefs.d.ts
+++ typings/prefs.d.ts
@@ -57,7 +57,6 @@ declare namespace _ZoteroTypes {
       "rule.correct-bookTitle-sentence-case": boolean;
       "rule.correct-proceedingsTitle-sentence-case": boolean;
       "rule.tool-update-metadata.confirm-when-item-type-change": boolean;
-      "rule.tool-clean-extra": boolean;
       "cleanExtra": boolean;
       "semanticScholarToken": string;
     };
