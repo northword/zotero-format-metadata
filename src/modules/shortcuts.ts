445bfe557592498c704582f5ed5e530e5607a3b6
Northword <northword@outlook.com>
2026-07-20 19:13:03 +0800

    tweaks

--- src/modules/shortcuts.ts
+++ src/modules/shortcuts.ts
@@ -1,19 +1,24 @@
 import { getPref } from "../utils/prefs";
 
-function defaultedGetPref(key: "shortcut.subscript" | "shortcut.supscript" | "shortcut.bold" | "shortcut.italic" | "shortcut.nocase" | "shortcut.lint", fallback: string): string {
-  const value = getPref(key);
-  return value ?? fallback;
-}
-
 export function registerShortcuts() {
   const SHORTCUTS = {
-    subscript: defaultedGetPref("shortcut.subscript", "accel,="),
-    supscript: defaultedGetPref("shortcut.supscript", "accel,+"),
-    bold: defaultedGetPref("shortcut.bold", "accel,B"),
-    italic: defaultedGetPref("shortcut.italic", "accel,I"),
-    nocase: defaultedGetPref("shortcut.nocase", "accel,N"),
-    lint: defaultedGetPref("shortcut.lint", "accel,alt,L"),
+    subscript: getPref("shortcut.subscript") ?? "accel,=",
+    supscript: getPref("shortcut.supscript") ?? "accel,shift,+",
+    bold: getPref("shortcut.bold") ?? "accel,B",
+    italic: getPref("shortcut.italic") ?? "accel,I",
+    nocase: getPref("shortcut.nocase") ?? "accel,N",
+    lint: getPref("shortcut.lint") ?? "accel,alt,L",
   };
+
+  if (Zotero.isMac) {
+    // `accel,shift,=` is for compatibility with macOS
+    if (SHORTCUTS.supscript === "accel,shift,+")
+      SHORTCUTS.supscript = "accel,shift,=";
+
+    // `accel,alt,¬` is for compatibility with macOS, because option+L will become `¬`
+    if (SHORTCUTS.lint === "accel,alt,L")
+      SHORTCUTS.lint = "accel,alt,¬";
+  }
 
   ztoolkit.Keyboard.register((ev, data) => {
     if (data.type !== "keyup" || !data.keyboard) {
@@ -24,8 +29,7 @@ export function registerShortcuts() {
       if (data.keyboard.equals(SHORTCUTS.subscript)) {
         addon.hooks.onShortcuts("subscript");
       }
-      // `accel,shift,=` is for compatibility with macOS
-      else if (data.keyboard.equals(SHORTCUTS.supscript) || data.keyboard.equals("accel,shift,=")) {
+      else if (data.keyboard.equals(SHORTCUTS.supscript)) {
         addon.hooks.onShortcuts("supscript");
       }
       else if (data.keyboard.equals(SHORTCUTS.bold)) {
@@ -39,8 +43,7 @@ export function registerShortcuts() {
       }
     }
 
-    // `accel,alt,¬` is for compatibility with macOS, because option+L will become `¬`
-    if (data.keyboard.equals(SHORTCUTS.lint) || data.keyboard.equals("accel,alt,¬")) {
+    if (data.keyboard.equals(SHORTCUTS.lint)) {
       addon.hooks.onLintInBatch("standard", "item");
     }
   });
